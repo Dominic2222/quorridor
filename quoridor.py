9f789105290b2f8ef7be9083d03d57510ddf897d
Dominic2222 <62488343+Dominic2222@users.noreply.github.com>
2020-03-29 16:59:23 -0400

    Update quoridor.py

--- quoridor.py
+++ quoridor.py
@@ -110,3 +110,30 @@ def afficher_damier_ascii(etat_jeu):
                 print(tableInterY1[str(9 - i)][str(j + 1)], end='')
             print('')
     print(_base_line())
+    
+    
+def main():
+    command = analyser_commande()
+    debut = api.lister_parties(command.idul)
+    pp.pprint(debut)
+    afficher_damier_ascii(debut['parties'][0]['état'])
+    for i in debut['parties']:
+        if i['id'] == id:
+            afficher_damier_ascii(i['état'])
+    while True:
+        print("Type de coup ['D', 'MH', 'MV']: ")
+        type_coup = input()
+        print("Position x: ")
+        position_x = int(input())
+        print("Position y: ")
+        position_y = int(input())
+        position = (position_x, position_y)
+        pp.pprint(api.jouer_coup(id, type_coup, position))
+        etat = api.lister_parties(command.idul)
+        for i in etat['parties']:
+            if i['id'] == id:
+                afficher_damier_ascii(i['état'])
+
+
+if __name__ == '__main__':
+    main()
